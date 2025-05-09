c5a6ae80075c9dbf80dd2f00f44d1f4581035b5b
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-05-09 23:36:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 // complete this js code
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+		
+	}
+	greet(){
+		return `Hello, my name is ${name}, I am ${age} years old.`
+	}
+}
+
+class Employee extends Person {
+	constructor(name, age, jobTitle) {
+		this.name = name;
+		this.age = age;
+		this.jobTitle = jobTitle;
+		
+	}
+	jobGreet(){
+		return `Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.`
+	}
+}
 function Person(name, age) {}
 
 function Employee(name, age, jobTitle) {}
