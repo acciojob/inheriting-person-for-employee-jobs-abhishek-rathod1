90fa0b9713f61586224ce2239078ce306b116287
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-05-10 15:30:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ class Person {
 		
 	}
 	greet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 		return `Hello, my name is ${this.name}, I am ${this.age} years old.`
 	}
 }
@@ -17,6 +18,7 @@ class Employee extends Person {
 		
 	}
 	jobGreet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 		return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
 	}
 }
